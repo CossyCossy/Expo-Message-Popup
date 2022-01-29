770f01e83be357d0a4dd16a29b85ecab36f9511d
CossyCossy <liqweedkosgey@gmail.com>
2022-01-29 21:47:33 +0300

    second commit

--- README.md
+++ README.md
@@ -1,3 +1,44 @@
 ## Popup Message 
 
-## This version if for react native applications
+# This version if for react native applications
+
+A simple and fully customizable React Native component that implements toast message popup. 
+* Pop up can be displayed on any position on the screen. 
+* Fully customizable UI.
+
+## Installation
+
+If using yarn:
+
+```
+yarn add messagepopup
+```
+
+If using npm:
+
+```
+npm i messagepopup
+```
+## Usage
+
+```
+import { MessagePopup } from 'messagepopup';
+```
+Simply place a `<MessagePopup />` tag for each desired step within the `<MessagePopup />` wrapper.
+
+```
+
+<MessagePopup
+    title={data.actionMessageTitle}
+    description={data.actionMessageDescription}
+    messageType={data.actionType}
+    icon={<SvgImage
+        containerStyle={{
+            width: `${25}px`,
+            height: `${25}px`,
+            icon_name: data.actionType === 'success' ? svgsIcons.success_icon : svgsIcons.clarity_error_icon
+        }}
+    />}
+/>
+
+```
---
+++ src/MessagePopup.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    Text,
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import { COLORS, SIZES } from '../constants';
+
+const MessagePopup = ({title, description, messageType, icon}) => {
+
+    const message_type = messageType ? messageType : 'success'
+
+    const message_title = title ? title : ''
+
+    const message_description = description ? description : ''
+
+    const message_icon = icon ? icon : null
+
+    return (
+        <Animatable.View
+            animation="fadeInDownBig"
+            style={{
+                width: SIZES.width,
+                position: 'absolute',
+                top: 0,
+                backgroundColor: message_type === 'success' ? COLORS.success : COLORS.error,
+                borderRadius: 5,
+                elevation: 10,
+            }}
+        >
+
+            <View
+                style={{
+                    marginTop: SIZES.padding * 1.5,
+                    marginBottom: SIZES.radius,
+                    marginHorizontal: SIZES.radius
+                }}
+            >
+
+                <View
+                    style={{
+                        flexDirection: 'row',
+                    }}
+                >
+                    <View style={{ marginTop: SIZES.radius * 0.5 }}>
+                        {/* <SvgImage
+                            containerStyle={{
+                                width: `${25}px`,
+                                height: `${25}px`,
+                                icon_name: icon_name
+                            }}
+                        /> */}
+                        {typeof message_icon !== "undefined" && message_icon !== null &&
+                            message_icon
+                        }
+                    </View>
+
+                    <View
+                        style={{
+                            marginLeft: SIZES.radius,
+                            marginRight: SIZES.radius
+                        }}
+                    >
+
+                        <Text
+                            style={{
+                                fontSize: 20,
+                                //...FONTS.semiBoldFont,
+                                lineHeight: 30
+                            }}
+                        >{message_title}</Text>
+
+                        <Text
+                            style={{
+                                fontSize: 16,
+                                //...FONTS.regularFont,
+                                lineHeight: 30,
+                                color: COLORS.black
+                            }}
+                        >{message_description}</Text>
+
+                    </View>
+                </View>
+
+            </View>
+
+
+        </Animatable.View>
+    )
+}
+
+export default MessagePopup;
